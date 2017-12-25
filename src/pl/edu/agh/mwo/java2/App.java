04813abe0d68e223ebf02a53ff9035b80833e5ce
Olimp Bockowski <bockowsk@student.agh.edu.pl>
2017-12-25 16:18:24 +0100

    An intial commit
    Two classes: App (main program) which depends on SudokuBoardChecker
    (includes whole logic and methods to verify boards' correctness.

---
+++ src/pl/edu/agh/mwo/java2/App.java
@@ -0,0 +1,41 @@
+package pl.edu.agh.mwo.java2;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class App {
+
+	public static void main(String[] args) {
+		/* 
+		 * 1. otworzyc worksheet 
+		 * 2. sprawdzic na kazdym sheet, czy plansza jest ok strukturalnie
+		 * 3. sprawdzic czy pola sa dobrze wypelnione (wg logiki sudoku)
+		 * */
+
+		// otwarcie sudoku.xlsx
+		try {
+			Workbook wb = WorkbookFactory.create(new File("sudoku.xlsx"));
+			SudokuBoardChecker sbc=new SudokuBoardChecker(wb);
+			// powinno sie pobrac liczbe sheet'ow, ale dla uproszczenia odczytuje
+			short sheetNumbers=7;
+			for (int sn=0;sn<sheetNumbers;sn++) {
+				boolean poprawnosc;
+				//CWICZENIE 1: Sprawdzanie poprawnosci kart
+				poprawnosc=sbc.verifyBoardStructure(sn);
+				//CWICZENIE 2: Sprawdzanie poprawnosci danych
+				boolean poprawnosc2=sbc.verifyBoard(sn);
+				// sheet number +1 dla czytelnosci (sa od zera, ale nazwane sa od 1)
+				String s1=String.format("sheet %d : poprawnosc syntaktyczna: %7b\tpoprawnosc danych: %7b\t ogolna poprawnosc: %7b", sn+1, poprawnosc, poprawnosc2, poprawnosc&poprawnosc2);
+				System.out.println(s1);
+			}
+
+		} catch (EncryptedDocumentException | InvalidFormatException | IOException e) {
+				e.printStackTrace();
+		}
+	}
+}
---
+++ src/pl/edu/agh/mwo/java2/SudokuBoardChecker.java
@@ -0,0 +1,173 @@
+package pl.edu.agh.mwo.java2;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+public class SudokuBoardChecker {
+
+	//skladowe
+	Workbook wb;
+	
+	// konstruktor
+	public SudokuBoardChecker(Workbook workbook) {
+		this.wb=workbook;
+	}
+	
+
+	//metody
+	public boolean verifyBoardStructure(int sheetIndex) {
+		// zmienna do sledzenia czy cokolwiek jest zle
+		boolean poprawnosc=true;
+		Sheet sheet = this.wb.getSheetAt(sheetIndex);
+		// LOGIKA:
+		// iteracja po wierszach, a wewnatrz iteracja po komorkach 
+		// 3 warunki, ktore moga zmienic zmienna poprawnosc: nie ma wiersza, nie ma komorki, komorka inna niz pusty lub cyfry  
+		
+		//iteracja po wierszach 
+		for (int wiersz=0;wiersz<9;wiersz++) {
+			// jesli tylko poprawnosc jest false, to porzuca dalsza prace
+			if (poprawnosc==false) break;
+			//przygotowanie row do kontroli
+			Row row = sheet.getRow(wiersz);
+			// w przypadku bledu sheet nie jest poprawny
+			if (row == null) {
+				poprawnosc=false;
+				break;
+			}
+			//wewnetrzna iteracja na komorkach
+			for (int c=0;c<9;c++) {
+				Cell cell=row.getCell(c);
+				// w przypadku bledu cell'a, nie jest poprawny
+				if (cell == null) {
+					poprawnosc=false;
+					break;
+				}
+				CellType cellType=cell.getCellTypeEnum();
+				if (!(cellType.equals(CellType.NUMERIC) || cellType.equals(CellType.BLANK))) {
+					poprawnosc=false;
+					break;
+				}
+			}
+			
+
+		}
+		
+		//koncowa wartosc
+		return poprawnosc;
+	}
+	
+	public boolean verifyBoard(int sheetIndex) {
+		
+		/* 
+		 * ALGORYTM:
+		 *  a) unikalne liczby w kazdej kolumnie (0-9)
+		 *  b) unikalne liczby w kazdym wierszu (0-9)
+		 *  c) unikalne liczby w kazdym kwadracie 3x3, (0-9 kwadratow)
+		 *   - algorytm dla kwadratow, kazdy kwadrat identifikuja wspolrzedne (x,y)
+		 *   - inkrementacja wiersza (x) od 0 do 3, przy stepie +3 dla wiersza
+		 *   - dla kazdego wiersza (x) inkrementacja kolumny (y) od 0 do 3 ze stepem +3 
+		 */
+		
+		// 3 zmienne logiczne, na podstawie ktorych bedzie zwracany wynik
+		boolean poprawnoscWierszy=true;
+		boolean poprawnoscKolumn=true;
+		boolean poprawnoscKwadratow=true;
+		
+		Sheet sheet = this.wb.getSheetAt(sheetIndex);
+		
+
+		// najpierw pobranie planszy
+		ArrayList<ArrayList<Cell>> listaWierszy=new ArrayList<ArrayList<Cell>>();
+		for (int wiersz=0;wiersz<9;wiersz++) {
+			// initializacja nested ArrayList
+			listaWierszy.add(new ArrayList<Cell>());
+			// pobranie wiersza
+			Row row = sheet.getRow(wiersz);
+			for (int komorka=0;komorka<9;komorka++) {
+				Cell cell = row.getCell(komorka);
+				listaWierszy.get(wiersz).add(cell);
+			}
+			
+		}
+		// 1. sprawdzenie poprawnosci wierszy
+		// pobieramy z kazdego wiersza wartosc i zapisujemy do ArrayList oraz HasSet
+		// na koniec porownujemy wielkosc dwoch collection, jak jest rozny, to poprawnoscWierszy false i break
+		
+		for (ArrayList<Cell> element: listaWierszy) {
+			// warunek na cel dalszego sprawdzania
+			if (poprawnoscWierszy == false) break;
+			ArrayList<Double> lista=new ArrayList<Double>();
+			HashSet<Double> zbior=new HashSet<Double>();
+			for (Cell c: element) {
+				CellType cellType = c.getCellTypeEnum();
+				if (cellType.equals(CellType.NUMERIC)) {
+					double value = c.getNumericCellValue();
+					lista.add(value);
+					zbior.add(value);
+				}
+			}
+			//porownanie czy sa powtorzenia
+			if (lista.size()!=zbior.size()) poprawnoscWierszy=false;
+		}
+		
+		// 2. sprawdzenie poprawnosci kolumn
+		for (int kolumna=0;kolumna<9;kolumna++) {
+			// warunek na cel dalszego sprawdzania
+			if (poprawnoscKolumn == false) break;
+			// collections do porownania
+			ArrayList<Double> listaKolumn=new ArrayList<Double>();
+			HashSet<Double> zbiorKolumn=new HashSet<Double>();
+			// dla i kolumn od 0 do 9 kolumn pobierz wszystkie wiersze, pobierz z nich element Cell pod indeksem i i zapisz do List i Set
+			for (ArrayList<Cell> element: listaWierszy) {
+				Cell c=element.get(kolumna);
+				CellType cellType = c.getCellTypeEnum();
+				if (cellType.equals(CellType.NUMERIC)) {
+					double value = c.getNumericCellValue();
+					listaKolumn.add(value);
+					zbiorKolumn.add(value);
+				}
+			//porownanie czy sa powtorzenia
+			if (listaKolumn.size()!=zbiorKolumn.size()) poprawnoscWierszy=false;
+			}
+		}
+		
+		// 3. sprawdzenie poprawnosci kwadratow
+		for (int w=0;w<9;w+=3) {
+			// warunek na cel dalszego sprawdzania
+			if (poprawnoscKwadratow==false) break;
+			for (int k=0;k<9;k+=3) {
+				// warunek na cel dalszego sprawdzania
+				if (poprawnoscKwadratow==false) break;
+				// rozpatrujemy tutaj konkretny kwardrat
+				ArrayList<Double> listaKwadrat=new ArrayList<Double>();
+				HashSet<Double> zbiorKwadrat=new HashSet<Double>();
+				for (int i=0;i<3;i++) {
+					for (int j=0;j<3;j++) {
+						// dla celow testowania
+						//String s=String.format("kwadrat: (%d,%d)\t wiersz: %d, kolumna: %d", w, k, w+i, k+j);
+						//System.out.println(s);
+						Cell c=listaWierszy.get(w+i).get(k+j);
+						CellType cellType = c.getCellTypeEnum();
+						if (cellType.equals(CellType.NUMERIC)) {
+							double value = c.getNumericCellValue();
+							listaKwadrat.add(value);
+							zbiorKwadrat.add(value);
+						}
+					}
+				}
+				//porownanie czy sa powtorzenia
+				if (listaKwadrat.size()!=zbiorKwadrat.size()) {
+					poprawnoscKwadratow=false;
+				}
+			}
+		}
+		
+		return (poprawnoscWierszy && poprawnoscKolumn && poprawnoscKwadratow);
+	}
+}
